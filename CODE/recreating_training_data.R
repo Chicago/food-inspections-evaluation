7cf1925966498a167d79d987cae107532cd8bd2e
Tom Schenk Jr <tomschenkjr@gmail.com>
2014-11-03 12:39:02 -0600

    Changed Rdata file to reflect most recent date

--- CODE/recreating_training_data.R
+++ CODE/recreating_training_data.R
@@ -206,4 +206,4 @@ evaluate <- subset(foodInspect, paste(inspection_id, license_, sep = "_") %in% p
 
 
 rm(list = setdiff(ls(),c('train','tune','evaluate')))
-save(list=ls(),file="./DATA/recreated_training_data_20141031v01.Rdata")
+save(list=ls(),file="./DATA/recreated_training_data_20141103v01.Rdata")
