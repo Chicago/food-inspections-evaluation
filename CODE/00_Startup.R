1a2600e3cf29d5c467c9c481c3d0e4c99edcc4cc
Gene Leynes <gleynes@gmail.com>
2015-02-07 16:43:56 -0600

    Changing 'ask' = FALSE for update.pacakges, and adding comments to 00_startup.  Without ask=FALSE the subsequent code appears in the (Y/N/c) questions for update.packages

--- CODE/00_Startup.R
+++ CODE/00_Startup.R
@@ -1,8 +1,16 @@
 ## INSTALL THESE DEPENDENCIES
 install.packages("devtools",dependencies = TRUE)
 install.packages("Rcpp",dependencies = TRUE)
-update.packages()
+
+## UPDATE PACKAGES
+## *NOTE* THIS WILL UPDATE YOUR R LIBRARIES AUTOMATICALLY
+## For most users this is fine, but if you may wish to skip this step
+## if you want to keep your current package versions, and update only 
+## if you experience problems.
+update.packages(ask = FALSE)
 
 ## Update two packages not on CRAN using the devtools package.
 devtools::install_github('geneorama/geneorama')
 devtools::install_github('yihui/printr')
+
+
