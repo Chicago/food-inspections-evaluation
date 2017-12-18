baa88b2a1dd81058b3f912aa7852485cc1ed2a0f
Gene Leynes <gleynes@gmail.com>
2017-12-18 15:16:53 -0600

    syncing and updating startup script, closes #101

--- CODE/00_Startup.R
+++ CODE/00_Startup.R
@@ -1,15 +1,55 @@
-## INSTALL THESE DEPENDENCIES
-install.packages("devtools",
-                 dependencies = TRUE,
-                 repos='http://cran.us.r-project.org')
-install.packages("Rcpp",
-                 dependencies = TRUE,
-                 repos='http://cran.us.r-project.org')
+##------------------------------------------------------------------------------
+## INSTALL DEPENDENCIES IF MISSING
+##------------------------------------------------------------------------------
 
-## Update two packages not on CRAN using the devtools package.
-devtools::install_github(repo = 'geneorama/geneorama')
-devtools::install_github(repo = 'yihui/printr')
+if(!"devtools" %in% rownames(installed.packages())){
+    install.packages("devtools",
+                     dependencies = TRUE,
+                     repos = "https://cloud.r-project.org/")
+}
+
+if(!"Rcpp" %in% rownames(installed.packages())){
+    install.packages("Rcpp",
+                     dependencies = TRUE,
+                     repos = "https://cloud.r-project.org/")
+}
+
+if(!"RSocrata" %in% rownames(installed.packages())){
+    install.packages("RSocrata",
+                     dependencies = TRUE,
+                     repos = "https://cloud.r-project.org/")
+}
+
+if(!"data.table" %in% rownames(installed.packages())){
+    install.packages("data.table",
+                     dependencies = TRUE,
+                     repos = "https://cloud.r-project.org/")
+}
+
+if(!"geneorama" %in% rownames(installed.packages())){
+    devtools::install_github('geneorama/geneorama')
+}
+
+if(!"printr" %in% rownames(installed.packages())){
+    devtools::install_github(repo = 'yihui/printr')
+}
+
+##------------------------------------------------------------------------------
+## UPDATE DEPENDENCIES IF MISSING
+##------------------------------------------------------------------------------
 
 ## Update to RSocrata 1.7.2-2 (or later) 
-## which is only on github as of March 8, 2016
-devtools::install_github(repo = 'chicago/RSocrata')
+if(installed.packages()["RSocrata","Version"] < "1.7.2-2"){
+    install.packages("RSocrata", 
+                     repos = "https://cloud.r-project.org/")
+}
+
+## Needs recent version for foverlaps
+if(installed.packages()["data.table","Version"] < "1.10.0"){
+    install.packages("data.table", 
+                     repos = "https://cloud.r-project.org/")
+}
+
+if(installed.packages()["geneorama","Version"] < "1.5.0"){
+    devtools::install_github('geneorama/geneorama')
+}
