36cbaabaa7b7724afe5d900cb6a69892ac3d8df5
Gene Leynes <gleynes@gmail.com>
2017-12-19 10:25:13 -0600

    updating filter functions to match model

--- CODE/functions/filter_foodInspect.R
+++ CODE/functions/filter_foodInspect.R
@@ -1,6 +1,13 @@
 filter_foodInspect <- function(foodInspect){
     foodInspect <- foodInspect[!is.na(Inspection_Date) & !is.na(License)]
-    foodInspect <- foodInspect[!duplicated(Inspection_ID)]
+    if(foodInspect[,any(duplicated(Inspection_ID))]){
+        warning(paste0("Removing ", nrow(foodInspect[duplicated(Inspection_ID)]), 
+                       " duplicated records from foodInspect ",
+                       "of ", nrow(foodInspect), " total records.\n",
+                       "Duplication is based on the Inspection_ID"))
+        ## Remove any duplicated Inspection_ID values
+        foodInspect <- foodInspect[!duplicated(Inspection_ID)]
+    }
     foodInspect <- foodInspect[License != 0]
     foodInspect <- foodInspect[Inspection_Date > as.IDate("2011-09-01")]
     foodInspect <- foodInspect[Inspection_Type == "Canvass"]
