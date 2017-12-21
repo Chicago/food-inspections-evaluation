9a1061680d510ba9c2a4cea89840dab2c94c9ce0
Gene Leynes <gleynes@gmail.com>
2017-12-21 16:43:14 -0600

    starting work described in #104 to split feature creation

---
+++ CODE/24_bus_features.R
@@ -0,0 +1,91 @@
+
+if(interactive()){
+    ##==========================================================================
+    ## INITIALIZE
+    ##==========================================================================
+    ## Remove all objects; perform garbage collection
+    rm(list=ls())
+    gc(reset=TRUE)
+    ## Detach any non-standard libraries
+    geneorama::detach_nonstandard_packages()
+}
+## Load libraries & project functions
+geneorama::loadinstall_libraries(c("data.table", "MASS"))
+geneorama::sourceDir("CODE/functions/")
+## Import shift function
+shift <- geneorama::shift
+
+##==============================================================================
+## LOAD CACHED RDS FILES
+##==============================================================================
+business <- readRDS("DATA/11_bus_license.Rds")
+
+## Apply filter to remove invalid / unused data
+business <- filter_business(business)
+
+## Food inspection data needed for some feature calculations inspection date
+foodInspect <- readRDS("DATA/23_food_insp_features.Rds")
+
+##==============================================================================
+## CALCULATE FEATURES BASED ON BUSINESS LICENSE DATA
+##==============================================================================
+
+## Calculate min date (by license)
+business[ , minDate := min(LICENSE_TERM_START_DATE), LICENSE_NUMBER]
+business[ , maxDate := max(LICENSE_TERM_EXPIRATION_DATE), LICENSE_NUMBER]
+
+##==============================================================================
+## Use only the business data that pertains to food inspections
+##==============================================================================
+## Create a table of matches between the food inspection and business license
+## data, based on the where the Inspection_Date falls within the business
+## license renewal
+id_table_food2business <- find_bus_id_matches(business, foodInspect)
+geneorama::NAsummary(id_table_food2business)
+
+## Add food key to matched business data
+bus_matched <- merge(x = id_table_food2business, 
+                     y = business, 
+                     by = "ID", 
+                     all.y = FALSE, 
+                     all.x = TRUE)
+setkey(bus_matched, Inspection_ID)
+
+## Add business key to food data
+foodInspect <- merge(x = id_table_food2business, 
+                     y = foodInspect, 
+                     by = "Inspection_ID")
+setkey(foodInspect, Inspection_ID)
+
+## Use minDate and Inspection date to calculate age at 
+bus_matched <- bus_matched[foodInspect[,Inspection_Date,keyby=Inspection_ID]]
+bus_matched[ , ageAtInspection := as.numeric(Inspection_Date - minDate) / 365]
+
+## Remove Inspection Date to avoid conflict names when merging later
+bus_matched[ , Inspection_Date := NULL]
+
+
+## CALCULATE AND MERGE IN OTHER CATEGORIES
+OtherCategories <- GenerateOtherLicenseInfo(foodInspect, business,  max_cat = 12)
+geneorama::NAsummary(OtherCategories)
+
+## Merge in results
+bus_matched <- merge(x = bus_matched, 
+                     y = OtherCategories, 
+                     by = "Inspection_ID",
+                     all.x = T)
+## Remove NAs in category columns and set max value to 1
+for (j in match(colnames(OtherCategories)[-1], colnames(bus_matched))) {
+    set(x = bus_matched, i = which(is.na(bus_matched[[j]])), j = j, value = 0)
+    set(x = bus_matched, j = j, value = pmin(bus_matched[[j]], 1))
+}
+
+bus_matched
+
+##==============================================================================
+## SAVE RDS
+##==============================================================================
+## Set the key for dat_model
+setkey(bus_matched, Inspection_ID)
+saveRDS(bus_matched, file.path("DATA/24_bus_features.Rds"))
+
--- CODE/functions/find_bus_id_matches.R
+++ CODE/functions/find_bus_id_matches.R
@@ -6,6 +6,16 @@
 ##
 
 find_bus_id_matches <- function(business, foodInspect) {
+    # browser()
+    ## Since many food businesses are inspected before they are issued a 
+    ## license,  so we need to move back the "License term start date" for 
+    ## newly issued licenses.  An adjustment of 365 days seems pretty 
+    ## reasonable, and creates lots of matches.
+    ## Note also, the `as.integer` call.  This is needed to avoid a warning.
+    business_copy <- copy(business)
+    business_copy <- business_copy[APPLICATION_TYPE=="ISSUE",
+                                   LICENSE_TERM_START_DATE := 
+                                       as.integer(LICENSE_TERM_START_DATE - 365)]
     
     ## Merge over time periods
     dat <- foverlaps(foodInspect[i = TRUE,
@@ -13,13 +23,13 @@ find_bus_id_matches <- function(business, foodInspect) {
                                  keyby = list(License, 
                                               Inspection_Date = Inspection_Date, 
                                               Inspection_Date_end = Inspection_Date)], 
-                     business[i = LICENSE_TERM_START_DATE < LICENSE_TERM_EXPIRATION_DATE, 
-                              j = list(Business_ID = ID),
-                              keyby = list(LICENSE_NUMBER, 
-                                           LICENSE_TERM_START_DATE, 
-                                           LICENSE_TERM_EXPIRATION_DATE)], 
+                     business_copy[i = LICENSE_TERM_START_DATE < LICENSE_TERM_EXPIRATION_DATE, 
+                                   j = list(ID),
+                                   keyby = list(LICENSE_NUMBER, 
+                                                LICENSE_TERM_START_DATE, 
+                                                LICENSE_TERM_EXPIRATION_DATE)], 
                      mult = "first", 
                      type = "any", 
                      nomatch = NA)
-    return(dat[ , list(Inspection_ID, Business_ID)])
+    return(dat[ , list(Inspection_ID, ID)])
 }
